629d5f5d8ca92916946832e644c4d6f657352103
stoftot <gryn@itu.dk>
2025-10-07 22:35:01 +0200

    Solved Natrij

---
+++ CSharpSolutions/Natrij/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var currentTime = TimeOnly.Parse(Console.ReadLine());
+        var timeOfExplosion = TimeOnly.Parse(Console.ReadLine());
+        var desiredTime = timeOfExplosion - currentTime;
+        if (desiredTime.Equals(TimeSpan.Zero)) Console.WriteLine("24:00:00");
+        else Console.WriteLine(desiredTime);
+    }
+}
